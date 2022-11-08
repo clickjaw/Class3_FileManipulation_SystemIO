0c05cea9c6aff0aca597a1d004af3808319c0dfd
Clickjaw <clickjaw@outlook.com>
2022-11-08 10:24:46 -0600

    working

--- FileManipulation_SystemIO/Program.cs
+++ FileManipulation_SystemIO/Program.cs
@@ -8,23 +8,28 @@ namespace FileManipulation_SystemIO
     {
         static void Main(String[] args)
         {
+            //===========adds to file ==========
             string filePath = "../new-data.txt";
+            string addCarFilePath = "../new-car-data.txt";
+            //========creates new file=============
             string newFilePath = "../new-data.txt";
+            // string carFilePath = "../new-car-data.txt";
 
             // Console.WriteLine("Hello World");
-
+            Console.Clear();
             FileReadAllTextAsBytes(filePath);
             FileReadAllText(filePath);
             FileReadAllTextAsArray(filePath);
-            FileAddSomeLines(filePath);
+            FileAddSomeLines(addCarFilePath);
             FileReadAllText(filePath);
-            FileCreateNewFile(newFilePath);
+            // FileCreateNewFile(newFilePath); //create new file
+            // FileCreateNewFile(carFilePath); //create new file
             // FileReadAllText(newFilePath);
         }
 
         static void FileCreateNewFile(string file)
         {
-            string[] words = { "I", "Don't", "Have", "The", "Power!" };
+            string[] words = { "I", "Have", "The", "Power!" };
             try
             {
                 using (StreamWriter sw = new StreamWriter(file))
@@ -34,7 +39,7 @@ namespace FileManipulation_SystemIO
                         foreach (string word in words)
                         {
                             sw.WriteLine(word);
-                            sw.WriteLine("\n");
+                            // sw.WriteLine("\n");
                         }
                     }
                     catch (Exception e)
@@ -54,41 +59,48 @@ namespace FileManipulation_SystemIO
             }
         }
 
-        static void FileReadAllTextAsBytes(string file){
+        static void FileReadAllTextAsBytes(string file)
+        {
             byte[] bytes = File.ReadAllBytes(file);
-            foreach (byte character in bytes){
+            foreach (byte character in bytes)
+            {
                 Console.Write($"{character}");
             }
         }
 
-        static void FileReadAllText(string file){
+        static void FileReadAllText(string file)
+        {
             Console.WriteLine("-----READ AS STRING-----");
             string myFileContents = File.ReadAllText(file);
             Console.WriteLine(myFileContents);
         }
 
-        static void FileReadAllTextAsArray(string file){
+        static void FileReadAllTextAsArray(string file)
+        {
             Console.WriteLine("-----Read as Array----");
             string[] lines = File.ReadAllLines(file);
-            foreach(string line in lines){
+            foreach (string line in lines)
+            {
                 Console.WriteLine(line);
             }
         }
 
-        static void FileAddSomeLines(string file){
+        static void FileAddSomeLines(string file)
+        {
             Console.WriteLine("------Add Lines-----");
-            string phrase = "The child likes his fish";
+            // string phrase = "He has more power than he thinks!\n";
+            string phrase = "He-Man needs BATTLECAT!\n";
             File.AppendAllText(file, "\n");
             File.AppendAllText(file, phrase);
 
-            string[] words = {
-                "sunny",
-                "hot",
-                "loud"
-            };
+            // string[] words = {
+            //     "sunny",
+            //     "hot",
+            //     "loud"
+            // };
 
-            File.AppendAllText(file, "\n");
-            File.AppendAllLines(file, words);
+            // File.AppendAllText(file, "\n");
+            // File.AppendAllLines(file, words);
 
             // File.Delete(file); // to delete the entire file
         }
